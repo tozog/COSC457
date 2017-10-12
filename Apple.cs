1b6408371e7ed454cf583aab3326ba0474093971
tozog <tozog@emich.edu>
2017-10-12 18:28:43 -0400

    Add files via upload

---
+++ Apple.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour {
+
+    public static float bottomY = -20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+
+        // Get a reference to the ApplePicker component of Main Camera
+        ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+           
+        // Call the public AppleDestroyed() method of apScript
+        apScript.AppleDestroyed();
+    }
+}
